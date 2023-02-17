4e8e32bee558f040bdff44eed4f73ff74aa01e18
Gitau Gakwa <50299997+gitaugakwa@users.noreply.github.com>
2023-02-17 14:13:28 +0300

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,36 @@
+project "Aubio"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/aubio.h",
+		"src/aubio_priv.h",
+		"src/cvec.c",
+		"src/fmat.c",
+		"src/fvec.c",
+    "src/lvec.c",
+		"src/mathutils.c",
+		"src/musicutils.c",
+		"src/vecutils.c"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
